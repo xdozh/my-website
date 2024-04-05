af0b751814d9ee6ac2fae24c76fe532201ff4723
Shourya Maithani <smaithani010@gmail.com>
2024-04-05 11:59:55 +0400

    V8

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,8 @@ import './index.css'
 import { SpeedInsights } from "@vercel/speed-insights/next"
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-    <SpeedInsights/>
+    <App>
+        <SpeedInsights/>
+    </App>,
   </React.StrictMode>,
 )
